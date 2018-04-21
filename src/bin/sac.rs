d3ba76b1da58c206629e38c9aa36023c455038c1
Arnau Siches <asiches@gmail.com>
2018-04-21 21:50:01 +0100

    feat(cli): Add item hash command
    Signed-off-by: Arnau Siches <asiches@gmail.com>

--- src/bin/sac.rs
+++ src/bin/sac.rs
@@ -6,26 +6,43 @@ extern crate log;
 
 extern crate sac;
 
-use sac::item;
+use std::process;
+use sac::{digest, item};
 
 use clap::{App, Arg, SubCommand};
 
 fn main() {
     let matches = App::new("sac")
         .version(crate_version!())
+        .author(crate_authors!())
         .about("Registers toolbelt")
-        .author("Arnau Siches")
         .subcommand(
             SubCommand::with_name("item")
                 .about("Manage items")
                 .subcommand(
                     SubCommand::with_name("canon")
+                        .aliases(&["fix"])
                         .about("Canonicalise item")
                         .arg(
                             Arg::with_name("input")
                                 .help("The item as JSON")
                                 .required(true)
                                 .index(1),
+                        ),
+                )
+                .subcommand(
+                    SubCommand::with_name("hash")
+                        .about("Hash item")
+                        .arg(
+                            Arg::with_name("input")
+                                .help("The item as JSON")
+                                .required(true)
+                                .index(1),
+                        )
+                        .arg(
+                            Arg::with_name("force")
+                                .help("Forces the item to be canonicalised")
+                                .long("force"),
                         ),
                 ),
         )
@@ -38,12 +55,45 @@ fn main() {
 
                 match itemr {
                     Ok(item) => println!("{}", item::to_json(&item).unwrap()),
-                    Err(err) => println!("{}", err.message),
+                    Err(err) => {
+                        println!("{}", err.message);
+                        process::exit(1)
+                    }
                 }
             }
+            ("hash", Some(hash_matches)) => {
+                let raw = hash_matches.value_of("input").unwrap();
+                let itemr = item::from_json(raw);
+
+                match itemr {
+                    Ok(item) => {
+                        let hash = item.hash();
+
+                        if hash_matches.is_present("force") {
+                            println!("{}", hash);
+                        } else {
+                            let raw_hash = digest::to_hex(digest::digest(raw).as_ref());
+
+                            if raw_hash == hash {
+                                println!("{}", hash);
+                            } else {
+                                eprintln!("The given item is not canonical");
+                                process::exit(1);
+                            }
+                        }
+                    }
+                    Err(err) => {
+                        eprintln!("{}", err);
+                    }
+                }
+            }
+
             _ => unimplemented!(),
         },
-
-        _ => unimplemented!(),
+        ("", None) => {
+            println!("No subcommand was used");
+            unimplemented!()
+        }
+        _ => unreachable!(),
     }
 }
