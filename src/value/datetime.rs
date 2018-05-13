7a5a0ae8dc8c68f887561067ea3f4dfe10a5e84c
Arnau Siches <asiches@gmail.com>
2018-05-13 11:13:43 +0100

    refactor(Datetime): Represent date branches as an enum of int
    Signed-off-by: Arnau Siches <asiches@gmail.com>

--- src/value/datetime.rs
+++ src/value/datetime.rs
@@ -4,7 +4,7 @@
 // at your option. This file may not be copied, modified, or distributed except
 // according to those terms.
 
-use regex::RegexSet;
+use regex::{Regex, RegexSet};
 use std::fmt::{self, Debug, Display};
 use super::Parse;
 
@@ -16,17 +16,40 @@ pub enum DatetimeError {
 
 /// ISO8601 Date time
 #[derive(Clone, PartialEq)]
-pub struct Datetime(String);
+pub enum Datetime {
+    Year(u16),
+    YearMonth(u16, u8),
+    Date(u16, u8, u8),
+    DateHour(u16, u8, u8, u8),
+    DateHourMinute(u16, u8, u8, u8, u8),
+    Full(u16, u8, u8, u8, u8, u8),
+}
 
 impl Debug for Datetime {
     fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-        formatter.debug_tuple("Datetime").field(&self.0).finish()
+        write!(formatter, "Datetime({})", &self)
     }
 }
 
 impl Display for Datetime {
     fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-        Display::fmt(&self.0, formatter)
+        match *self {
+            Datetime::Year(ref y) => write!(formatter, "{:04}", y),
+            Datetime::YearMonth(ref y, ref m) => write!(formatter, "{:04}-{:02}", y, m),
+            Datetime::Date(ref y, ref m, ref d) => write!(formatter, "{:04}-{:02}-{:02}", y, m, d),
+            Datetime::DateHour(ref y, ref m, ref d, ref h) => {
+                write!(formatter, "{:04}-{:02}-{:02}T{:02}Z", y, m, d, h)
+            }
+            Datetime::DateHourMinute(ref y, ref m, ref d, ref h, ref mm) => {
+                write!(formatter, "{:04}-{:02}-{:02}T{:02}:{:02}Z", y, m, d, h, mm)
+            }
+
+            Datetime::Full(ref y, ref m, ref d, ref h, ref mm, ref s) => write!(
+                formatter,
+                "{:04}-{:02}-{:02}T{:02}:{:02}:{:02}Z",
+                y, m, d, h, mm, s
+            ),
+        }
     }
 }
 
@@ -36,18 +59,60 @@ impl Parse for Datetime {
     type Err = DatetimeError;
     fn parse(s: &str) -> Result<Self, Self::Err> {
         lazy_static! {
-            static ref RE: RegexSet = RegexSet::new(&[
-                r"^\d{4}$",
-                r"^\d{4}-\d{2}$",
-                r"^\d{4}-\d{2}-\d{2}$",
-                r"^\d{4}-\d{2}-\d{2}T\d{2}Z$",
-                r"^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}Z$",
-                r"^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$",
-            ]).unwrap();
+            static ref RE_SET: Vec<Regex> = vec![
+                Regex::new(r"^(?P<year>\d{4})$").unwrap(),
+                Regex::new(r"^(?P<year>\d{4})-(?P<month>\d{2})$").unwrap(),
+                Regex::new(r"^(?P<year>\d{4})-(?P<month>\d{2})-(?P<day>\d{2})$").unwrap(),
+                Regex::new(r"^(?P<year>\d{4})-(?P<month>\d{2})-(?P<day>\d{2})T(?P<hour>\d{2})Z$").unwrap(),
+                Regex::new(r"^(?P<year>\d{4})-(?P<month>\d{2})-(?P<day>\d{2})T(?P<hour>\d{2}):(?P<minute>\d{2})Z$").unwrap(),
+                Regex::new(r"^(?P<year>\d{4})-(?P<month>\d{2})-(?P<day>\d{2})T(?P<hour>\d{2}):(?P<minute>\d{2}):(?P<second>\d{2})Z$").unwrap(),
+            ];
+
+            static ref RE: RegexSet = RegexSet::new(RE_SET.iter().map(|re| re.as_str())).unwrap();
         }
 
-        if RE.is_match(s) {
-            Ok(Datetime(s.to_owned()))
+        let ms: Vec<_> = RE.matches(s).into_iter().collect();
+        if !ms.is_empty() {
+            let idx = ms[0];
+            let caps = RE_SET[idx].captures(s).unwrap();
+
+            match idx {
+                0 => Ok(Datetime::Year((&caps["year"]).parse().unwrap())),
+
+                1 => Ok(Datetime::YearMonth(
+                    (&caps["year"]).parse().unwrap(),
+                    (&caps["month"]).parse().unwrap(),
+                )),
+
+                2 => Ok(Datetime::Date(
+                    (&caps["year"]).parse().unwrap(),
+                    (&caps["month"]).parse().unwrap(),
+                    (&caps["day"]).parse().unwrap(),
+                )),
+                3 => Ok(Datetime::DateHour(
+                    (&caps["year"]).parse().unwrap(),
+                    (&caps["month"]).parse().unwrap(),
+                    (&caps["day"]).parse().unwrap(),
+                    (&caps["hour"]).parse().unwrap(),
+                )),
+                4 => Ok(Datetime::DateHourMinute(
+                    (&caps["year"]).parse().unwrap(),
+                    (&caps["month"]).parse().unwrap(),
+                    (&caps["day"]).parse().unwrap(),
+                    (&caps["hour"]).parse().unwrap(),
+                    (&caps["minute"]).parse().unwrap(),
+                )),
+                5 => Ok(Datetime::Full(
+                    (&caps["year"]).parse().unwrap(),
+                    (&caps["month"]).parse().unwrap(),
+                    (&caps["day"]).parse().unwrap(),
+                    (&caps["hour"]).parse().unwrap(),
+                    (&caps["minute"]).parse().unwrap(),
+                    (&caps["second"]).parse().unwrap(),
+                )),
+
+                _ => unreachable!(),
+            }
         } else {
             Err(DatetimeError::ParseError)
         }
@@ -60,7 +125,7 @@ mod tests {
 
     #[test]
     fn parse_year() {
-        let expected = r#"Ok(Datetime("2018"))"#.to_string();
+        let expected = r#"Ok(Datetime(2018))"#.to_string();
         let actual = Datetime::parse("2018");
 
         assert_eq!(format!("{:?}", actual), expected);
@@ -68,7 +133,7 @@ mod tests {
 
     #[test]
     fn parse_yearmonth() {
-        let expected = r#"Ok(Datetime("2018-01"))"#.to_string();
+        let expected = r#"Ok(Datetime(2018-01))"#.to_string();
         let actual = Datetime::parse("2018-01");
 
         assert_eq!(format!("{:?}", actual), expected);
@@ -76,7 +141,7 @@ mod tests {
 
     #[test]
     fn parse_date() {
-        let expected = r#"Ok(Datetime("2018-01-01"))"#.to_string();
+        let expected = r#"Ok(Datetime(2018-01-01))"#.to_string();
         let actual = Datetime::parse("2018-01-01");
 
         assert_eq!(format!("{:?}", actual), expected);
@@ -84,7 +149,7 @@ mod tests {
 
     #[test]
     fn parse_datehour() {
-        let expected = r#"Ok(Datetime("2018-01-01T10Z"))"#.to_string();
+        let expected = r#"Ok(Datetime(2018-01-01T10Z))"#.to_string();
         let actual = Datetime::parse("2018-01-01T10Z");
 
         assert_eq!(format!("{:?}", actual), expected);
@@ -92,7 +157,7 @@ mod tests {
 
     #[test]
     fn parse_datehourminute() {
-        let expected = r#"Ok(Datetime("2018-01-01T10:11Z"))"#.to_string();
+        let expected = r#"Ok(Datetime(2018-01-01T10:11Z))"#.to_string();
         let actual = Datetime::parse("2018-01-01T10:11Z");
 
         assert_eq!(format!("{:?}", actual), expected);
@@ -100,7 +165,7 @@ mod tests {
 
     #[test]
     fn parse_datetime() {
-        let expected = r#"Ok(Datetime("2018-01-01T10:11:12Z"))"#.to_string();
+        let expected = r#"Ok(Datetime(2018-01-01T10:11:12Z))"#.to_string();
         let actual = Datetime::parse("2018-01-01T10:11:12Z");
 
         assert_eq!(format!("{:?}", actual), expected);
